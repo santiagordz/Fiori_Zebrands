404ea340d7c5beed45df97995315a3f8b6e2e55a
Santiago Rodriguez Murialdo <santiromur@gmail.com>
2023-03-10 11:59:32 -0600

    Insert Usuario Etiqueta

---
+++ sql/inserts/insert_usuario_etiqueta.sql
@@ -0,0 +1,20 @@
+INSERT INTO `usuario_etiqueta` (`id_usuario_etiqueta`, `id_usuario`, `id_etiqueta`) VALUES
+(3, 'ana.castillo@zeb.mx', 22),
+(4, 'ana.gonzalez@zeb.mx', 23),
+(5, 'ana.rojas@zeb.mx', 39),
+(6, 'carlos.rodriguez@zeb.mx', 40),
+(7, 'david.martinez@zeb.mx', 26),
+(8, 'emilio.gonzalez@zeb.mx', 31),
+(9, 'fernando.lopez@zeb.mx', 27),
+(10, 'jane.doe@zeb.mx', 36),
+(11, 'john.smith@zeb.mx', 21),
+(12, 'jorge.lopez@zeb.mx', 30),
+(13, 'juan.ramirez@zeb.mx', 37),
+(14, 'laura.sanchez@zeb.mx', 29),
+(15, 'liliana.perez@zeb.mx', 38),
+(16, 'luis.hernandez@zeb.mx', 32),
+(18, 'maria.garcia@zeb.mx', 25),
+(19, 'martha.ortiz@zeb.mx', 24),
+(20, 'sara.perez@zeb.mx', 35),
+(21, 'sofia.ramirez@zeb.mx', 34);
+
