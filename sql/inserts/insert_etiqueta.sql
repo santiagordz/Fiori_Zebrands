7f999377f728033c5f8b5057b24845b308ab292d
Santiago Rodriguez Murialdo <santiromur@gmail.com>
2023-03-09 22:41:39 -0600

    Update insert_etiqueta.sql

--- sql/inserts/insert_etiqueta.sql
+++ sql/inserts/insert_etiqueta.sql
@@ -1,42 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 5.2.0
--- https://www.phpmyadmin.net/
---
--- Servidor: localhost
--- Tiempo de generación: 10-03-2023 a las 04:27:26
--- Versión del servidor: 10.4.24-MariaDB
--- Versión de PHP: 8.1.6
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Base de datos: `Zebrands`
---
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `etiqueta`
---
-
-CREATE TABLE `etiqueta` (
-  `id_etiqueta` int(11) NOT NULL,
-  `nombre_etiqueta` varchar(50) NOT NULL,
-  `color_etiqueta` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Volcado de datos para la tabla `etiqueta`
---
-
 INSERT INTO `etiqueta` (`id_etiqueta`, `nombre_etiqueta`, `color_etiqueta`) VALUES
 (1, 'front-end', 'rojo'),
 (2, 'back-end', 'verde'),
@@ -58,28 +19,3 @@ INSERT INTO `etiqueta` (`id_etiqueta`, `nombre_etiqueta`, `color_etiqueta`) VALU
 (18, 'machine-learning', 'rojo'),
 (19, 'big-data', 'azul'),
 (20, 'blockchain', 'amarillo');
-
---
--- Índices para tablas volcadas
---
-
---
--- Indices de la tabla `etiqueta`
---
-ALTER TABLE `etiqueta`
-  ADD PRIMARY KEY (`id_etiqueta`);
-
---
--- AUTO_INCREMENT de las tablas volcadas
---
-
---
--- AUTO_INCREMENT de la tabla `etiqueta`
---
-ALTER TABLE `etiqueta`
-  MODIFY `id_etiqueta` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
