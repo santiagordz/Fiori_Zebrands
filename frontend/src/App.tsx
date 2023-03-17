ff6f9a7b48c48c774a13d8ad1acdfc4b004552b3
Santiago Rodriguez Murialdo <santiromur@gmail.com>
2023-03-17 16:33:33 -0600

    Before new branch

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,10 @@
 import TableWrapper from './components/administrar-usuarios/TableWrapper';
 import UsersTable from './components/administrar-usuarios/UsersTable';
-import RolIcon from './components/administrar-usuarios/RolIcon';
 
 function App() {
   return (
     <>
       <TableWrapper />
-      <RolIcon />
     </>
   );
 }
