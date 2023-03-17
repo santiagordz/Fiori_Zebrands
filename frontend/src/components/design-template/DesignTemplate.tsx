82283b7bdc8af80c44cdc3a9e9660318148db248
David Langarica <david.langarica@gmail.com>
2023-03-17 17:53:09 -0600

    add comments

--- frontend/src/components/design-template/DesignTemplate.tsx
+++ frontend/src/components/design-template/DesignTemplate.tsx
@@ -1,3 +1,4 @@
+// Importa las dependencias necesarias para el componente DesignTemplate
 import React, { FC } from 'react';
 import Sidebar from './Sidebar';
 import HomeIcon from '@atlaskit/icon/glyph/home';
@@ -9,11 +10,13 @@ import PeopleIcon from '@atlaskit/icon/glyph/people';
 import { useLocation } from 'react-router-dom';
 import type { GlyphProps } from '@atlaskit/icon/types';
 
+// Definimos las propiedades necesarias para el designtemplate
 interface DesignTemplateProps {
   children: React.ReactNode;
   buttons?: JSX.Element;
 }
 
+// Definimos un tipo de objeto que contiene a desplegar según la ruta
 interface pathInfoType {
   [key: string]: {
     view: string;
@@ -21,6 +24,7 @@ interface pathInfoType {
   };
 }
 
+// Definimos los iconos y los nombres para cada ruta
 const pathInfo: pathInfoType = {
   '/': {
     view: 'Dashboard',
@@ -30,7 +34,6 @@ const pathInfo: pathInfoType = {
     view: 'Métricas',
     icon: GraphBarIcon,
   },
-
   'mis-retrospectivas': {
     view: 'Mis retrospectivas',
     icon: PdfIcon,
@@ -49,41 +52,48 @@ const pathInfo: pathInfoType = {
   },
 };
 
+// Definimos un componente de tipo función que recibe las props DesignTemplateProps
 const DesignTemplate: FC<DesignTemplateProps> = ({
   children,
   buttons,
 }) => {
+  // Obtiene la ruta actual
   const location = useLocation().pathname;
+  // Obtiene la primera parte de la ruta (antes del primer '/')
   const path = '/' + location.split('/')[1];
+  // Definimos un nombre por defecto
   const name = 'Santiago';
+  // Obtiene el icono que corresponde a la ruta actual
   const MainIcon: React.ComponentType<GlyphProps> =
     pathInfo[path].icon;
   return (
     <>
       <div className="flex flex-col items-center gap-7 w-auto h-screen mx-[6vw] my-[6vh] lg:ml-[9vw] lg:mr-[3vw]">
-        <Sidebar
-          idRol={1}
-          view={'Dashboard'}
-          name={'Santiago Rodríguez'}
-        />
-        <p className="font-medium text-xl w-full text-left">
+        {/* Incluimos el componente Sidebar */}
+        <Sidebar idRol={1} name={'Santiago Rodríguez'} />
+        {/* Mostramos un saludo con el nombre definido */}
+        <p className="font-medium text-xl w-full text-left text-paragraph">
           Hola, {name}
         </p>
         <div className="flex justify-between items-center w-full mt-3">
           <div className="flex items-center gap-4">
+            {/* Mostramos el icono de la ruta actual */}
             <div className="bg-selectBold flex p-2 rounded-xl w-fit">
               <MainIcon
                 label={`${pathInfo[path].view.toLowerCase()}`}
                 primaryColor="white"
               />
             </div>
-            <h1 className="font-semibold text-2xl">
+            {/* Mostramos el título de la vista actual */}
+            <h1 className="font-semibold text-2xl text-accentBolder">
               {pathInfo[path].view}
             </h1>
           </div>
+          {/* Mostramos los botones opcionales que se pasan como prop */}
           <div className="flex gap-2">{buttons}</div>
         </div>
-        <div className="bg-background w-full h-auto rounded-lg p-7">
+        {/* Mostramos el contenido principal del componente */}
+        <div className="bg-background w-full h-auto rounded-lg p-7 text-paragraph">
           {children}
         </div>
       </div>
@@ -91,4 +101,5 @@ const DesignTemplate: FC<DesignTemplateProps> = ({
   );
 };
 
+// Exporta el componente DesignTemplate por defecto
 export default DesignTemplate;
--- frontend/src/components/design-template/Sidebar.tsx
+++ frontend/src/components/design-template/Sidebar.tsx
@@ -1,5 +1,5 @@
+// Importamos las librerías y componentes necesarios para el sidebar
 import React, { FC, useState } from 'react';
-
 import Avatar from '@atlaskit/avatar';
 import HomeIcon from '@atlaskit/icon/glyph/home';
 import GraphBarIcon from '@atlaskit/icon/glyph/graph-bar';
@@ -10,13 +10,20 @@ import PeopleIcon from '@atlaskit/icon/glyph/people';
 import SignOutIcon from '@atlaskit/icon/glyph/sign-out';
 import MenuIcon from '@atlaskit/icon/glyph/menu';
 import { Link, useLocation } from 'react-router-dom';
-
 import { motion } from 'framer-motion';
 
+// Importamos la imagen del logo de la empresa
 import zebrandsLogo from '../../assets/zebrandsLogo.svg';
 
+// Definimos las propiedades necesarias para el sidebar
+interface SidebarProps {
+  idRol: number;
+  name: string;
+}
+
+// Creamos una lista con las categorías y la información necesaria de cada una
 const categories = [
-  { name: 'Dashboard', icon: HomeIcon, path: '', exact: true },
+  { name: 'Dashboard', icon: HomeIcon, path: '' },
   {
     name: 'Métricas',
     icon: GraphBarIcon,
@@ -46,12 +53,7 @@ const categories = [
   },
 ];
 
-interface SidebarProps {
-  idRol: number;
-  view: string;
-  name: string;
-}
-
+// Definimos los estilos para los botones y texto en el sidebar
 const buttonActiveStyles =
   'bg-[#E9F2FF] px-[2vmin] py-[0.8vmin] flex justify-start w-full items-center rounded-md gap-5';
 
@@ -67,13 +69,16 @@ const textNotActiveStyles = `${textStyle} text-paragraph`;
 
 const textActiveStyles = `${textStyle} text-selectBold`;
 
-const Sidebar: FC<SidebarProps> = ({ idRol, view, name }) => {
+// Creamos el componente Sidebar
+const Sidebar: FC<SidebarProps> = ({ idRol, name }) => {
+  // Definimos los estados necesarios para el sidebar
   const [toggleSidebar, setToggleSidebar] = useState<boolean>(false);
   const location = useLocation();
   const pColor = '#44546F';
   const sColor = '#0C66E4';
   const iconSize = 'medium';
 
+  // Definimos las animaciones necesarias para el sidebar
   const sidebarVariants = {
     hovered: {
       width: '21vw',
@@ -94,14 +99,17 @@ const Sidebar: FC<SidebarProps> = ({ idRol, view, name }) => {
     collapsed: { width: '70%' },
   };
 
+  // Renderizamos el componente Sidebar
   return (
     <>
+      {/* Creamos un botón que se muestra solo en dispositivos móviles */}
       <button
         className="lg:hidden w-full"
         onClick={() => setToggleSidebar(true)}
       >
         <MenuIcon label="menu" primaryColor={pColor} size={'large'} />
       </button>
+      {/* Creamos el sidebar con animaciones */}
       <motion.div
         layout
         initial="collapsed"
@@ -111,6 +119,7 @@ const Sidebar: FC<SidebarProps> = ({ idRol, view, name }) => {
         className={`invisible overflow-hidden whitespace-nowrap flex flex-col bg-white h-[100vh] items-center drop-shadow py-12 fixed left-0 lg:visible top-0`}
       >
         <div className="flex flex-col justify-between h-full">
+          {/* Mostramos el avatar del usuario y su nombre */}
           <div className="flex items-center justify-center gap-3 border-gray-100 border-solid border-2 bg-white rounded-full py-[0.2vmin] w-full">
             <Avatar />
             <motion.p
@@ -120,6 +129,8 @@ const Sidebar: FC<SidebarProps> = ({ idRol, view, name }) => {
               {name}
             </motion.p>
           </div>
+
+          {/* Mostramos las categorías del sidebar */}
           <div className="flex flex-col justify-center items-center gap-7 w-full">
             {categories.map((category) => {
               const Icon = category.icon;
@@ -135,6 +146,7 @@ const Sidebar: FC<SidebarProps> = ({ idRol, view, name }) => {
               const showLink =
                 !category.roleIds || category.roleIds.includes(idRol);
 
+              // Mostramos el link solo si el usuario tiene los permisos necesarios
               return showLink ? (
                 <Link
                   className={activeStyles}
@@ -155,6 +167,8 @@ const Sidebar: FC<SidebarProps> = ({ idRol, view, name }) => {
               ) : null;
             })}
           </div>
+
+          {/* Creamos un botón para cerrar sesión */}
           <button className={`${buttonStyles} justify-center`}>
             <SignOutIcon
               label="cerrar sesión"
@@ -169,6 +183,8 @@ const Sidebar: FC<SidebarProps> = ({ idRol, view, name }) => {
             </motion.p>
           </button>
         </div>
+
+        {/* Mostramos el logo de la empresa */}
         <motion.img
           src={zebrandsLogo}
           alt="zebrands"
@@ -180,4 +196,5 @@ const Sidebar: FC<SidebarProps> = ({ idRol, view, name }) => {
   );
 };
 
+// Exportamos el componente Sidebar
 export default Sidebar;
