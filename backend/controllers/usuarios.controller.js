468904e9ca8ec9db62c406909948ef0f24da1f91
Santiago Rodriguez Murialdo <santiromur@gmail.com>
2023-03-17 16:32:15 -0600

    usuarios controller update

--- backend/controllers/usuarios.controller.js
+++ backend/controllers/usuarios.controller.js
@@ -3,7 +3,6 @@ const Usuario = require('../models/usuarios.model');
 exports.getUsuarios = (req, res, next) => {
   Usuario.getAllUsuarios()
     .then(([rows, fieldData]) => {
-      console.log(rows);
       res.json(rows);
     })
     .catch((err) => console.log(err));
