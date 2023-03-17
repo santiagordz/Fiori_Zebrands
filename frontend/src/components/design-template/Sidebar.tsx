be7526393a0bcf581f05a2accb3ff256f88d6202
David Langarica <david.langarica@gmail.com>
2023-03-16 19:48:45 -0600

    hover buttons

--- frontend/src/components/design-template/Sidebar.tsx
+++ frontend/src/components/design-template/Sidebar.tsx
@@ -13,55 +13,75 @@ interface SidebarProps {
   idRol: number;
 }
 
+const buttonActiveStyles =
+  'bg-[#E9F2FF] p-1 flex justify-center items-center rounded-md';
+
+const buttonAndHover =
+  'flex items-center justify-center p-1 rounded-md hover:bg-[#E9F2FF]';
+
+const transitionWidthStyle =
+  'transition-width duration-300 ease-in-out';
+
+const transitionTextStyle =
+  'transition-opacity duration-300 ease-in-out';
+
 const Sidebar: FC<SidebarProps> = ({ idRol }) => {
-  const [sidebarHovered, setSidebarHovered] = useState<boolean>(true);
+  const [sidebarHovered, setSidebarHovered] =
+    useState<boolean>(false);
   const pColor = '#44546F';
   const sColor = '#0C66E4';
   const iconSize = 'medium';
+  const views = 'Dashboard';
 
   const name = 'Santiago Sánchez';
 
   return (
     <div
       onMouseEnter={() => setSidebarHovered(true)}
-      // onMouseLeave={() => setSidebarHovered(false)}
-      className={`invisible flex flex-col bg-white justify-between ${
+      onMouseLeave={() => setSidebarHovered(false)}
+      className={`invisible flex flex-col bg-white justify-between transition-width duration-300 ease-in-out ${
         sidebarHovered ? `w-[18vw]` : `w-[6vw]`
       } h-[100vh] items-center drop-shadow py-14 fixed left-0 lg:visible top-0`}
     >
       <div className="flex items-center justify-center gap-3">
         <Avatar />
         <p
-          className={`${
-            sidebarHovered ? 'block' : 'hidden'
+          className={`${transitionTextStyle} ${
+            sidebarHovered ? 'block opacity-1' : 'hidden opacity-0'
           } font-bold text-[2vmin]`}
         >
           {name}
         </p>
       </div>
       <div className="flex flex-col justify-between items-center gap-10">
-        <button className="bg-[#E9F2FF] p-1 flex justify-center items-center rounded-md">
+        <button
+          className={`${
+            views.toLowerCase() === 'dashboard'
+              ? buttonActiveStyles
+              : buttonAndHover
+          }`}
+        >
           <HomeIcon
             label="home"
             primaryColor={sColor}
             size={iconSize}
           />
         </button>
-        <button>
+        <button className={buttonAndHover}>
           <GraphBarIcon
             label="graph"
             primaryColor={pColor}
             size={iconSize}
           />
         </button>
-        <button>
+        <button className={buttonAndHover}>
           <PdfIcon
             label="pdf"
             primaryColor={pColor}
             size={iconSize}
           />
         </button>
-        <button>
+        <button className={buttonAndHover}>
           <StarFilledIcon
             label="star"
             primaryColor={pColor}
@@ -69,7 +89,7 @@ const Sidebar: FC<SidebarProps> = ({ idRol }) => {
           />
         </button>
         {(idRol === 1 || idRol === 3) && (
-          <button>
+          <button className={buttonAndHover}>
             <SwitcherIcon
               label="switcher"
               primaryColor={pColor}
@@ -78,7 +98,7 @@ const Sidebar: FC<SidebarProps> = ({ idRol }) => {
           </button>
         )}
         {idRol === 1 && (
-          <button>
+          <button className={buttonAndHover}>
             <PeopleIcon
               label="people"
               primaryColor={pColor}
@@ -87,7 +107,7 @@ const Sidebar: FC<SidebarProps> = ({ idRol }) => {
           </button>
         )}
       </div>
-      <button>
+      <button className={buttonAndHover}>
         <SignOutIcon
           label="signOut"
           primaryColor={pColor}
