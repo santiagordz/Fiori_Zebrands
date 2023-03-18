75f41c3093dab06121d9c58cdb7ea730be89efa8
Santiago Rodriguez Murialdo <santiromur@gmail.com>
2023-03-18 16:39:21 -0600

    CU Consulta Usuario

--- backend/controllers/usuarios.controller.js
+++ backend/controllers/usuarios.controller.js
@@ -1,4 +1,3 @@
-const e = require('express');
 const Usuario = require('../models/usuarios.model');
 
 exports.getUsuarios = (req, res, next) => {
@@ -9,44 +8,14 @@ exports.getUsuarios = (req, res, next) => {
     .catch((err) => console.log(err));
 };
 
-exports.getUsuariosConRoles = (req, res, next) => {
+exports.fetchUsuarios = async (req, res, next) => {
   try {
-    Usuario.getUsuariosConRolesMasImportante()
-      .then(([rows, fieldData]) => {
-        res.json(rows);
-      })
-      .catch((err) => console.log(err));
-  } catch {
+    const usuarios = await Usuario.fetchUsuarios();
+    res.json({ usuarios });
+  } catch (error) {
+    console.error(error);
     res
       .status(500)
-      .json({ message: 'Error al obtener los usuarios con roles' });
-  }
-};
-
-exports.getUsuariosConEtiquetas = (req, res, next) => {
-  try {
-    Usuario.getUsuariosConEtiquetas()
-      .then(([rows, fieldData]) => {
-        res.json(rows);
-      })
-      .catch((err) => console.log(err));
-  } catch {
-    res.status(500).json({
-      message: 'Error al obtener los usuarios con etiquetas',
-    });
-  }
-};
-
-exports.getUsuariosRolEtiqueta = (req, res, next) => {
-  try {
-    Usuario.getUsuariosRolEtiqueta()
-      .then(([rows, fieldData]) => {
-        res.json(rows);
-      })
-      .catch((err) => console.log(err));
-  } catch {
-    res.status(500).json({
-      message: 'Error al obtener los usuarios con roles y etiquetas',
-    });
+      .json({ message: 'Error al obtener los usuarios.' });
   }
 };
--- backend/routes/usuarios.routes.js
+++ backend/routes/usuarios.routes.js
@@ -2,12 +2,7 @@ const express = require('express');
 const router = express.Router();
 const usuariosController = require('../controllers/usuarios.controller');
 
-router.get('/roles', usuariosController.getUsuariosConRoles);
-router.get('/etiquetas', usuariosController.getUsuariosConEtiquetas);
-router.get(
-  '/roles-etiquetas',
-  usuariosController.getUsuariosRolEtiqueta
-);
+router.get('/info', usuariosController.fetchUsuarios);
 router.get('/', usuariosController.getUsuarios);
 
 module.exports = router;
