ed3bf8c573a065b030506d0b3975a613a41de70b
Randu Retana <randu2002@hotmail.com>
2023-03-18 18:35:11 -0600

    Agrega modal

--- frontend/src/components/administrar-usuarios/views/RegistrarUsuarios.views.tsx
+++ frontend/src/components/administrar-usuarios/views/RegistrarUsuarios.views.tsx
@@ -1,98 +1,24 @@
-// import React, { FC, useCallback, useState } from 'react'
-// import Modal, {
-//     ModalBody,
-//     ModalFooter,
-//     ModalHeader,
-//     ModalTitle,
-//     ModalTransition,
-//   } from '@atlaskit/modal-dialog';
-//   import Button from '@atlaskit/button';
+import React, { FC } from 'react'
+import '../css/modal.css'
 
-
-
-// const RegistrarUsuarios = ({  }) => {
-//     const [isOpen, setisOpen] = useState(false)
-//     const openModal = useCallback(() => setisOpen(true), [])
-//     const closeModal = useCallback(() => setisOpen(false), [])
-
-//     return (
-//         <div>
-//         <Button onClick={openModal}>Open Modal</Button>
-//         <ModalTransition>
-//             {isOpen && (
-//                 <Modal onClose={closeModal}>
-//                     <ModalHeader>
-//                         <ModalTitle>Registar Usuario</ModalTitle>
-//                     </ModalHeader>
-//                     <ModalBody>
-//                         <p>Modal body</p>
-//                     </ModalBody>
-//                     <ModalFooter>
-//                         <button onClick={closeModal}>Close</button>
-//                         <button onClick={closeModal}>Save</button>
-//                     </ModalFooter>
-//                 </Modal>    
-//             )}
-//         </ModalTransition>
-//         </div>
-// )}
-
-import React, { useCallback, useRef, useState } from 'react';
-
-import Breadcrumbs, { BreadcrumbsItem } from '@atlaskit/breadcrumbs';
-import Button from '@atlaskit/button/standard-button';
-import { Field } from '@atlaskit/form';
-import Textfield from '@atlaskit/textfield';
-
-import Modal, {
-  ModalBody,
-  ModalFooter,
-  ModalHeader,
-  ModalTransition,
-} from '@atlaskit/modal-dialog';
-
-export default function Example() {
-  const [isOpen, setIsOpen] = useState(false);
-  const openModal = useCallback(() => setIsOpen(true), []);
-  const closeModal = useCallback(() => setIsOpen(false), []);
-  const focusRef = useRef<HTMLElement>();
-
-  return (
-    <>
-      <Button appearance="primary" onClick={openModal}>
-        Open modal
-      </Button>
-
-      <ModalTransition>
-        {isOpen && (
-          <Modal autoFocus={focusRef} onClose={closeModal}>
-            <ModalHeader>
-              <Breadcrumbs>
-                <BreadcrumbsItem href="#" text="Projects" />
-                <BreadcrumbsItem href="#" text="Design System Team" />
-              </Breadcrumbs>
-            </ModalHeader>
-            <ModalBody>
-              <Field label="Email" name="my-email" defaultValue="">
-                {({ fieldProps }) => (
-                  <Textfield
-                    ref={focusRef}
-                    autoComplete="off"
-                    placeholder="ian@atlas.com"
-                    {...fieldProps}
-                  />
-                )}
-              </Field>
-            </ModalBody>
-            <ModalFooter>
-              <Button appearance="subtle">Learn more</Button>
-              <Button appearance="primary" onClick={closeModal} autoFocus>
-                Sign up
-              </Button>
-            </ModalFooter>
-          </Modal>
-        )}
-      </ModalTransition>
-    </>
-  );
+interface RegistrarUsuariosProps {
+  
 }
+
+const RegistrarUsuarios: FC<RegistrarUsuariosProps> = ({  }) => {
+  return (
+    <div className='modal'>
+        <div className='modal-content'>
+            <div className='modal-header'>
+                <h4 className='modal-title'>Registrar Usuarios</h4>
+            </div>
+            <div className='modal-body'>
+            </div>
+            <div className='modal-footer'>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default RegistrarUsuarios;
