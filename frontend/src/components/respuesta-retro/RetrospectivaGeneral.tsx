b2985c42bab44f2391f65ffd1a12acdf78deb1ba
JP-coder2000 <70972830+JP-coder2000@users.noreply.github.com>
2023-03-19 21:14:27 -0600

    Vistas

--- frontend/src/components/respuesta-retro/RetrospectivaGeneral.tsx
+++ frontend/src/components/respuesta-retro/RetrospectivaGeneral.tsx
@@ -1,5 +1,7 @@
 import React, { FC } from 'react'
-import FlagFilledIcon from '@atlaskit/icon/glyph/flag-filled'
+import  FlagFilledIcon from '@atlaskit/icon/glyph/flag-filled'
+import { SimpleTag as Tag } from '@atlaskit/tag';
+import ChevronRightLargeIcon from '@atlaskit/icon/glyph/chevron-right-large'
 
 interface RetrospectivaGeneralProps {
   
@@ -7,8 +9,25 @@ interface RetrospectivaGeneralProps {
 
 const RetrospectivaGeneral: FC<RetrospectivaGeneralProps> = ({  }) => {
   return (
-    <div className='flex '> {/* Aqui se crea el contenedor de la tarjeta */}
-      
+    <div className='flex py-6 '> {/* Div de todo lo que va adentro de la parte azul */}
+      <div className='flex flex-col py-3 px-5 w-full rounded bg-white border border-solid border-gray-300 border-collapse'> {/* Div del rectángulo blanco */}
+        <div className='flex w-full justify-between '> {/* Div de la parte superior */}
+          <div className='gap-4 flex flex-row'>{/* Div del Icono y título */}
+            <FlagFilledIcon label='retrospectiva-pendiente' primaryColor='#8270DB' />
+            <h3 className='font-bold'>Retrospectiva Sprint 3</h3>
+          </div>
+          <div className='flex flex-row gap-4 ml-auto'>{/* Div de el tag */}
+            <Tag text='back-end' appearance='rounded' color='yellowLight'/>
+          </div>
+        </div>
+        <div className='flex py-5 text-sm'> {/* Div del texto  */}
+          <p>La retrospectiva tiene como fin evaluar el rendimiento tanto personal como del equipo en el Sprint 3, tomar en cuenta tu desempeño durante todo el periodo.</p>
+        </div>
+        <div className='flex flex-row text-xs justify-between'> {/* Div de la parte de hasta abajo */}
+          <p>Fecha de Inicio: 07/Marzo/2023 </p>
+          <ChevronRightLargeIcon label='flecha' primaryColor='#1D7AFC' />
+        </div>
+      </div>
     </div>
   )
 }
--- frontend/src/views/mis-retrospectivas/MisRetrospectivas.tsx
+++ frontend/src/views/mis-retrospectivas/MisRetrospectivas.tsx
@@ -1,13 +1,16 @@
 import React, { FC } from 'react';
-import { DesignTemplate, RetrospectivaGeneral } from '../../components'; {/* De esta manera mandamos a llamar a los componentes */}
+import { DesignTemplate, RetrospectivaGeneral } from '../../components';
+import ResponderRetro from '../../components/respuesta-retro/ResponderRetro';
 
 interface MisRetrospectivasProps { }
 
-const MisRetrospectivas: FC<MisRetrospectivasProps> = ({}) => {
+const MisRetrospectivas: FC<MisRetrospectivasProps> = ({ }) => {
   return (
     <DesignTemplate>
-      <h2 className='text-lg font-bold text-information'>Retrospectivas pendientes de responder</h2>
-      <RetrospectivaGeneral />  {/* De esta manera mandamos a llamar a los componentes */}
+      {/* <h2 className='text-lg font-bold text-information'>Retrospectivas pendientes de responder</h2>
+      <RetrospectivaGeneral /> 
+      <h2 className='text-lg font-bold text-information'> Otras Retrospectivas</h2> */}
+      <ResponderRetro />
     </DesignTemplate>
   );
 };
