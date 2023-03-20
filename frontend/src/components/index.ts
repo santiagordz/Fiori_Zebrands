f3dd43c1f70b10680f01f8c2d2ad65eac32706c7
David Langarica <david.langarica@gmail.com>
2023-03-20 09:12:35 -0600

    fix routes

--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -1,3 +1,1 @@
 export { default as DesignTemplate } from './design-template/DesignTemplate';
-export { default as RetrospectivaGeneral } from './respuesta-retro/RetrospectivaGeneral';
-export { default as PanelRetros } from './respuesta-retro/PanelRetros';
--- frontend/src/views/mis-retrospectivas/MisRetrospectivas.tsx
+++ frontend/src/views/mis-retrospectivas/MisRetrospectivas.tsx
@@ -1,27 +1,12 @@
 import React, { FC } from 'react';
-import {
-  DesignTemplate,
-  PanelRetros,
-  RetrospectivaGeneral,
-} from '../../components';
-import ResponderRetro from '../../components/respuesta-retro/ResponderRetro';
-import { retrospective } from '../../components/respuesta-retro/RetroDomi';
-import { Routes, Route } from 'react-router-dom';
-import NotFound404 from '../NotFound404';
+import { DesignTemplate } from '../../components';
 
 interface MisRetrospectivasProps {}
 
 const MisRetrospectivas: FC<MisRetrospectivasProps> = ({}) => {
   return (
     <DesignTemplate>
-      <Routes>
-        <Route path="*" element={<NotFound404 />} />
-        <Route path="/" element={<PanelRetros />} />
-        <Route
-          path="/responder/:retroId"
-          element={<ResponderRetro />}
-        />
-      </Routes>
+      <DesignTemplate>Estoy en Mis Retrospectivas</DesignTemplate>;
     </DesignTemplate>
   );
 };
