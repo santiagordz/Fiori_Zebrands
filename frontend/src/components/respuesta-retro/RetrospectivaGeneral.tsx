e262835dd69584ccdac8cd60d73667d7fd2660c6
David Langarica <david.langarica@gmail.com>
2023-03-20 10:17:26 -0600

    borra lineas raras

--- frontend/src/components/respuesta-retro/RetrospectivaGeneral.tsx
+++ frontend/src/components/respuesta-retro/RetrospectivaGeneral.tsx
@@ -42,10 +42,8 @@ const RetrospectivaGeneral: FC<RetrospectivaGeneralProps> = ({
     >
       {/* Div de todo lo que va adentro de la parte azul */}
       <div className="flex flex-col py-3 px-5 w-full rounded bg-white border border-solid border-gray-300 border-collapse">
-        {' '}
         {/* Div del rectángulo blanco */}
         <div className="flex w-full justify-between ">
-          {' '}
           {/* Div de la parte superior */}
           <div className="gap-4 flex flex-row">
             {/* Div del Icono y título */}
@@ -65,12 +63,10 @@ const RetrospectivaGeneral: FC<RetrospectivaGeneralProps> = ({
           </div>
         </div>
         <div className="flex py-5 text-sm">
-          {' '}
           {/* Div del texto  */}
           <p>{descripcion}</p>
         </div>
         <div className="flex flex-row text-xs justify-between">
-          {' '}
           {/* Div de la parte de hasta abajo */}
           <p>Fecha de inicio: {fechaInicio}</p>
           {!isInResponder && (
