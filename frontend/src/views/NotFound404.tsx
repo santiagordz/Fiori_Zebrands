bf9ec11be984163836afb55d4e945675dd8bdc6e
David Langarica <david.langarica@gmail.com>
2023-03-20 09:19:48 -0600

    retro fix

--- frontend/src/views/NotFound404.tsx
+++ frontend/src/views/NotFound404.tsx
@@ -1,9 +1,14 @@
 import React, { FC } from 'react';
+import { Sidebar } from '../components';
 
 interface NotFound404Props {}
 
 const NotFound404: FC<NotFound404Props> = ({}) => {
-  return <div className="">NotFound404</div>;
+  return (
+    <div className="">
+      <Sidebar />
+    </div>
+  );
 };
 
 export default NotFound404;
