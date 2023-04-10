280947fafb7a9349425e6a61a4f3b168ac373340
FridaBG2002 <A01708633@itesm.mx>
2023-04-10 11:01:29 -0600

    useMemo

--- frontend/src/components/respuestas-resumen/Resultados.tsx
+++ frontend/src/components/respuestas-resumen/Resultados.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from "react";
+import React, { FC, useMemo } from "react";
 
 interface ResultadosProps {
   id_tipo_pregunta: number;
@@ -11,14 +11,28 @@ interface ResultadosProps {
   }[];
 }
 
-const Resultados: FC<ResultadosProps> = ({}) => {
+const Resultados: FC<ResultadosProps> = ({
+  id_tipo_pregunta,
+  pregunta,
+  respuestas,
+}) => {
+  const Case = useMemo(() => {
+    switch (id_tipo_pregunta) {
+      case 3:
+        return <div>Respuestas de tipo 3</div>;
+      case 4:
+        return <div>Respuestas de tipo 4</div>;
+      default:
+        return <div>Respuestas de tipo default</div>;
+    }
+  }, [id_tipo_pregunta, pregunta, respuestas]);
   return (
     <div
       className={`flex flex-col py-3 px-5 w-full gap-1 rounded bg-white 
     shadow-sm
     }`}
     >
-      Resultados
+      {Case}
     </div>
   );
 };
