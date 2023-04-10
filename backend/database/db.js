8903efd79b0e059cbd22d7c4a5429cec0319a179
David Langarica <david.langarica@gmail.com>
2023-04-10 16:09:49 -0600

    colors chart

--- backend/database/db.js
+++ backend/database/db.js
@@ -1,11 +1,10 @@
-const mysql = require("mysql2");
+const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  database: "fiori",
-  password: "root",
-  port: 8889,
+  host: 'localhost',
+  user: 'root',
+  database: 'fiori',
+  password: '',
 });
 
 module.exports = pool.promise();
