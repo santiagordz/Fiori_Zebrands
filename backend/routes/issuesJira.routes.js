74c3e7dbed69719f839d111a8663761e14a50138
Santiago Rodriguez Murialdo <santiromur@gmail.com>
2023-04-17 12:05:58 -0600

    modelo para actualizar issues

--- backend/routes/issuesJira.routes.js
+++ backend/routes/issuesJira.routes.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const issuesJira_controller = require('../controllers/issues.controller');
 
+
+router.get('/updated', issuesJira_controller.fetchIssuesJiraUpdated);
 router.get('/', issuesJira_controller.getIssuesJira);
 router.post('/', issuesJira_controller.postIssuesJira);
 
