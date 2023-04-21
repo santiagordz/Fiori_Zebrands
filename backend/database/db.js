326468398bd442b865d6900dd845271a7049267c
Santiago Rodriguez Murialdo <santiromur@gmail.com>
2023-04-20 21:10:14 -0600

    add db connection message

--- backend/database/db.js
+++ backend/database/db.js
@@ -8,3 +8,11 @@ const pool = mysql.createPool({
 });
 
 module.exports = pool.promise();
+
+pool.query('SELECT 1', (error, results) => {
+  if (error) {
+    console.error('Error connecting to the database:', error);
+  } else {
+    console.log('Connected to the database successfully');
+  }
+});
