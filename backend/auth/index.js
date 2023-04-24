dd31b16e5d0b74a175a5c84886b9327a884d7aad
David Langarica <david.langarica@gmail.com>
2023-04-24 12:29:26 -0600

    app.js

--- backend/auth/index.js
+++ backend/auth/index.js
@@ -3,6 +3,6 @@ const router = express.Router();
 
 const loginWithGoogleApi = require('./loginWithGoogle');
 
-router.use('/api', loginWithGoogleApi);
+router.use('/api/', loginWithGoogleApi);
 
 module.exports = router;
