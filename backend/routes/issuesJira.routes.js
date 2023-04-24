1a34546e0ac23542990a2f8a0e6c33326627223d
David Langarica <david.langarica@gmail.com>
2023-04-24 15:28:00 -0600

    fix jira routes

--- backend/routes/issuesJira.routes.js
+++ backend/routes/issuesJira.routes.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const issuesJira_controller = require('../controllers/issues.controller');
 
 router.get('/updated', issuesJira_controller.fetchIssuesJiraUpdated);
-router.get('/', issuesJira_controller.getIssuesJira);
-router.post('/', issuesJira_controller.postIssuesJira);
+router.get('/api', issuesJira_controller.getIssuesJira);
+router.post('/api', issuesJira_controller.postIssuesJira);
 router.get('/last-fetch', issuesJira_controller.getLastFetch);
 
 module.exports = router;
